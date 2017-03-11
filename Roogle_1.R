b2062d001119cf3132ae09d2983ecfd05b6dcea4
olgnaydn <olgunaydinn@gmail.com>
2017-03-11 03:37:02 +0300

    Roogle Package
    The application for connecting Google Cloud Vision API.

---
+++ Roogle_1.R
@@ -0,0 +1,42 @@
+install.packages("RoogleVision", repos = c(getOption("repos"), "http://cloudyr.github.io/drat"))
+devtools::install_github("MarkEdmondson1234/googleAuthR")
+install.packages("googleAuthR")
+require(googleAuthR)
+require(bitops)
+require(jsonlite)
+require(magrittr)
+require(stringi)
+require(RoogleVision)
+
+### credentials
+options("googleAuthR.client_id" = "629611377311-5lmp1oddb0g27trms7un81oavgebt5a9.apps.googleusercontent.com")
+options("googleAuthR.client_secret" = "xxxx")
+
+
+devtools::install_github("cloudyr/RoogleVision",force=TRUE)
+
+
+#getting images urls
+images_url <- read.csv(file = "images.csv", header = TRUE, sep = ",")
+images_url <- images_url$source_url
+images_url <- as.matrix(images_url)
+
+
+
+
+## use Google Auth R package
+
+options("googleAuthR.scopes.selected" = c("https://www.googleapis.com/auth/cloud-platform"))
+googleAuthR::gar_auth()
+
+############
+#Sendin image to API
+o <- getGoogleVisionResponse("http://image.xxxx.jpg", feature="LABEL_DETECTION")
+
+
+### FEATURES
+# with the parameter 'feature' you can define which type of analysis you want. Results differ by feature-type.
+# The default is set to 'LABEL_DETECTION' but you can choose one out of: 
+#FACE_DETECTION, LANDMARK_DETECTION, LOGO_DETECTION, LABEL_DETECTION, TEXT_DETECTION		
+
+
