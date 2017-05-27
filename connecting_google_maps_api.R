ae028bbcd9ece1e76d33b3ee20cd69508c70c259
Olgun AYDIN <olgunaydinn@gmail.com>
2017-05-27 18:15:28 +0300

    Put extra information

--- connecting_google_maps_api.R
+++ connecting_google_maps_api.R
@@ -1,6 +1,8 @@
-
+#setting loop. Dont forget that Google Maps API's daily request size is 2500!
 r <- dim(stations)[1]
 
+
+#setting variables will be used.
 get_stations <- NULL
 name<- NULL
 district <- NULL
@@ -11,11 +13,14 @@ lon <- NULL
 geo_info <- NULL
 get_stations_final <- NULL
 
-for (i in 1:1000)
+#setting loop connecting Google Maps API and getting data.
+for (i in 1:r)
 {
-  get_stations <- fromJSON(getURL(paste('https://maps.googleapis.com/maps/api/geocode/json?address=',str_replace_all(as.matrix(stations$DURAK)[i]," ", "+"),'&key=AIzaSyCXgoUvFzFBDmW0eeLjXLrOEvqa8uMsFtQ',sep="")))
-  #name<- get_stations$results$address_components[[1]][str_detect( get_stations$results$address_components[[1]][,3], 'route'),2]
-  if(length(which(str_detect( get_stations$results$address_components[[1]][,3], 'administrative_area_level_4')))==0)
+  #connecting Google Maps API using jsonlite and RCurl library.
+  get_stations <- fromJSON(getURL(paste('https://maps.googleapis.com/maps/api/geocode/json?address=',str_replace_all(as.matrix(stations$from)[i]," ", "+"),'YOUR_KEY',sep="")))
+
+  #getting district name of location
+  if(length(which(str_detect(get_stations$results$address_components[[1]][,3], 'administrative_area_level_4')))==0)
   {
     district <- "N/A"
   }
@@ -24,6 +29,7 @@ for (i in 1:1000)
   district <- get_stations$results$address_components[[1]][which(str_detect( get_stations$results$address_components[[1]][,3], 'administrative_area_level_4')),2]
   }
   
+  #getting county name of location
   if(length(which(str_detect( get_stations$results$address_components[[1]][,3], 'administrative_area_level_2')))==0)
   {
     county <- "N/A"
@@ -32,6 +38,8 @@ for (i in 1:1000)
   {
   county <- get_stations$results$address_components[[1]][which(str_detect( get_stations$results$address_components[[1]][,3], 'administrative_area_level_2')),2]
   }
+  
+  #getting city name of location
   if(length(which(str_detect( get_stations$results$address_components[[1]][,3], 'administrative_area_level_1')))==0)
   {
     city <- "N/A"
@@ -41,6 +49,7 @@ for (i in 1:1000)
   city <- get_stations$results$address_components[[1]][which(str_detect( get_stations$results$address_components[[1]][,3], 'administrative_area_level_1')),2]
   }
   
+  #checking API response status and getting lat-long values.
   if(get_stations$status=='ZERO_RESULTS')
   {
   lat <- "N/A"
@@ -52,6 +61,9 @@ for (i in 1:1000)
   lon <- get_stations$results$geometry$location$lng
   }
   
+  #concating all variables
   geo_info <- data.frame(district,county,city,lat,lon)
+  
+  #assigning each response to a variable
   get_stations_final <- rbind(get_stations_final,geo_info)
 }
