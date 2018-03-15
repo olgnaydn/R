790875639b5a2ec4f5a2a893e93e3d09ca304759
Olgun AYDIN <olgunaydinn@gmail.com>
2018-03-15 16:12:14 +0100

    Adding comments

--- connection.R
+++ connection.R
@@ -1,15 +1,16 @@
-
+#calling library
 library("RPostgreSQL")
 
+#Loading PostgreSQL driver
 drv <- dbDriver("PostgreSQL")
+
+#connecting to db
 con <- dbConnect(drv, dbname = "development", host = "development.clgjf7569y1t.us-west-2.rds.amazonaws.com", port = 5432,
                  user = "olgun", password = 'xxx')
 
 
-
-
-#tablodan select etme
+#select operation from a table)
 d<-dbGetQuery(con,"select * from test2")
 
-#tabloya yazdirma
+#write operation to a table
 dbWriteTable(con, "test", d)
